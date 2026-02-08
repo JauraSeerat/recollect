bc2e4f615a787655e5ec71d655a6d4171dbe44c3
Seerat Jaura <46867415+JauraSeerat@users.noreply.github.com>
2026-02-08 14:53:48 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app = FastAPI(title="Notes App API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
